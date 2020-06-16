54a91e2bd1a50f950b9c8b771aa99ca081a72418
Veslum <Frederik.Veslum@dnvgl.com>
2020-06-16 12:35:27 +0200

    assignment3_main is the main file

---
+++ assignment3_main/Class/card.py
@@ -0,0 +1,36 @@
+class Card(object):
+    '''
+    Input: char and int
+    - Gives every card in a cardstock group and value
+    output: List with all the cards 
+    '''
+    char = ['spade','cluc','heart','diamond']
+    value = list(range(1,14))
+    print(value)
+    def __init__(self,char,value):
+        self.char = char
+        self.value = value
+    
+    def __str__(self):
+        return(f'The chosen card is {char} of {value}')
+    
+    def getSuit(self):
+        return self.char
+
+    def getFace(self):
+        return self.value
+    
+    def toString(self):
+        return self.value,self.char
+
+
+
+
+'''
+if __name__ == "__main__":
+    char = 'heart'
+    value = 1
+    #print(card(char,value))
+    face = card(char,value).toString()
+    print(f'{char} of {value}')
+'''    
