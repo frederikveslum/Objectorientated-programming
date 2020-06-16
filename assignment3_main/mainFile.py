58a66a39fdc36d42da77b238e25644d57f3578dc
Veslum <Frederik.Veslum@dnvgl.com>
2020-06-16 14:18:10 +0200

    import class

--- assignment3_main/mainFile.py
+++ assignment3_main/mainFile.py
@@ -1,40 +1,11 @@
-'''
-class card(object):
-    '''
-    #Input: char and int
-    #- Gives every card in a cardstock group and value
-    #output: List with all the cards 
-'''
-char = ['spade','cluc','heart','diamond']
-value = list(range(1,14))
-print(value)
-def __init__(self,char,value):
-    self.char = char
-    self.value = value
-
-def __str__(self):
-    return(f'The chosen card is {char} of {value}')
-
-def getSuit(self):
-    return self.char
-
-def getFace(self):
-    return self.value
-
-def toString(self):
-    return self.value,self.char
-'''
-#from Class import card
-#import Class.card as myClass
-from Class.card import Card as myClassCard
+from Class.Card import Card as myClassCard
 
 #Class.card as myClass
 if __name__ == "__main__":
     char = 'heart'
     value = 1
-    #print(card(char,value))
-    face = myClassCard(char,value).toString()
-    print(f'{char} of {value}')
-    #face = myClassCard(char,value).getFace()
-    #print(face)
+    toStringTuple = myClassCard(char,value).toString()
+    #print(f'{char} of {value}')
+    print(f'{toStringTuple[1]} of {toStringTuple[0]}')
+
     
