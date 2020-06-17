52412bebc6780dcaf43f16da139bea2f88ac73a1
Veslum <Frederik.Veslum@dnvgl.com>
2020-06-17 11:09:55 +0200

    The class CardDeck has been implemented

--- assignment3_main/mainFile.py
+++ assignment3_main/mainFile.py
@@ -1,11 +1,17 @@
 from Class.Card import Card as myClassCard
+from Class.CardDeck import CardDeck as myClassCardDeck
 
 #Class.card as myClass
 if __name__ == "__main__":
-    char = 'heart'
-    value = 1
-    toStringTuple = myClassCard(char,value).toString()
-    #print(f'{char} of {value}')
-    print(f'{toStringTuple[1]} of {toStringTuple[0]}')
+    '''
+    For testing the Card Class
+    #char = 'heart'
+    #value = 1
+    #toStringTuple = myClassCard(char,value).toString()
+    ##print(f'{char} of {value}')
+    #print(f'{toStringTuple[1]} of {toStringTuple[0]}')
+    '''
+    #Testing ClassCard Class
+    
 
     
