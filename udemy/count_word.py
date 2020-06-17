47eafd5333f66c26ce83ef8bb8c4f4da0bbf758b
Veslum <Frederik.Veslum@dnvgl.com>
2020-06-17 13:53:42 +0200

    The read from .txt file did not work

--- udemy/count_word.py
+++ udemy/count_word.py
@@ -1,13 +1,15 @@
 '''
 Function which counts each word in a string
 '''
+#f = open('count_word_check.txt', 'r')
+#my_str = f.read()
 def count_func(my_str):
+#def count_func(my_str):
     my_str_list = list(my_str.split())
     length = len(my_str_list)
     return length
 
-
 if __name__ == "__main__":
-    my_str = "Hello world, nice weather outside!"
+    #my_str = "Hello world, nice weather outside!"
     my_count_func = count_func(my_str)
     print(my_count_func)
